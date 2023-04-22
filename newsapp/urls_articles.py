2f9ba98875cae85b6178c67be738e97508a96ef4
Alexander Zakharov <zakharov.alex.a@mail.ru>
2023-04-22 13:34:36 +0700

    Change urls 'edit'

--- newsapp/urls_articles.py
+++ newsapp/urls_articles.py
@@ -3,6 +3,6 @@ from newsapp.views import ArticlesCreate, ArticlesUpdate, ArticlesDelete
 
 urlpatterns = [
     path('create/', ArticlesCreate.as_view(), name='create_articles'),
-    path('<int:pk>/update', ArticlesUpdate.as_view(), name='update_articles'),
+    path('<int:pk>/edit', ArticlesUpdate.as_view(), name='update_articles'),
     path('<int:pk>/delete', ArticlesDelete.as_view(), name='delete_articles'),
 ]
--- newsapp/urls_news.py
+++ newsapp/urls_news.py
@@ -7,7 +7,7 @@ urlpatterns = [
     # int — указывает на то, что принимаются только целочисленные значения
     path('<int:pk>', PostDetail.as_view(), name='post'),
     path('create/', PostCreate.as_view(), name='create_news'),
-    path('<int:pk>/update', PostUpdate.as_view(), name='update_news'),
+    path('<int:pk>/edit', PostUpdate.as_view(), name='update_news'),
     path('<int:pk>/delete', PostDelete.as_view(), name='delete_news'),
 ]
 
